742ffb36f2abbcad05297a55857171d49bb333c0
zeeshanriasat302 <zeeshan.riasat302@gmail.com>
2024-01-03 12:38:07 +0500

    Revert "stripe test mode frontend integration"
    This reverts commit 2fcd236e0ee262dab865a59230531eb5ad7f0308.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
-import StripeCheckout from "react-stripe-checkout";
-import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [product, setProduct] = useState({ 
-    name: "React from FB", 
-    price: 10,
-    productBy: "facebook"
-  });
-
-  const makePayment = (token) => {
-    const body = {
-      token,
-      product
-    }
-
-    const headers = {
-      "Content-Type": "application/json"
-    }
-    return fetch(`http://localhost:5000/payment`, {
-      method: "POST",
-      headers,
-      body: JSON.stringify(body)
-    }).then(
-      response => {
-        console.log("RESPONSE: ", response);
-        const {status} = response;
-        console.log("STATUS ",status);
-      }
-    ).catch(error => console.log("error --> ", error))
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
-          href="#"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-        <StripeCheckout 
-          stripeKey={process.env.REACT_APP_STRIPE_SECRET_KEY}
-          token={makePayment}
-          name="By React"
-          amount={product.price * 100}
-        >
-        <button className = "btn-large pink">Buy react in just {product.price}</button>
-        </StripeCheckout>
       </header>
     </div>
   );
